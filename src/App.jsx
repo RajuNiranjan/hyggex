32b7cd1fdaede486d534a79e77736aee12288b77
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-17 02:08:45 +0530

    created telation function ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import Navbar from "./components/navbar";
 import Navgation from "./components/navgation";
 import CreateFlashCard from "./components/createFlashCard";
 import Faq from "./components/faq";
+import RelationFunction from "./components/relationfunction";
 
 const App = () => {
   return (
     <div className="container m-auto px-10">
-      <Navbar />
-      <Navgation />
-      <CreateFlashCard />
-      <Faq />
+      <div className="mt-[px]">
+        <Navbar />
+      </div>
+      <div className="mt-[px]">
+        <Navgation />
+      </div>
+      <div className="mt-[px]">
+        <RelationFunction />
+      </div>
+      <div className="mt-[px]">
+        <CreateFlashCard />
+      </div>
+      <div className="mt-[px]">
+        <Faq />
+      </div>
     </div>
   );
 };
--- src/ui/button.jsx
+++ src/ui/button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <button className="bg-gradient- bg-gradient-to-b from-[#06286e] to-[#164ec0] text-white flex px-10 py-3 rounded-full capitalize">
+    <button className="bg-gradient-to-b from-[#06286e] to-[#164ec0] text-white flex px-10 py-3 rounded-full capitalize">
       {props.name}
     </button>
   );
