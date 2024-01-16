5ca6b2825b53c434f41aec89939b9634b30caf09
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-16 23:53:31 +0530

    created navgation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Navgation from "./components/navgation";
+import CreateFlashCard from "./components/createFlashCard";
+import Faq from "./components/faq";
 
 const App = () => {
   return (
-    <div className="container m-auto">
+    <div className="container m-auto px-10">
       <Navbar />
+      <Navgation />
+      {/* <CreateFlashCard /> */}
+      {/* <Faq /> */}
     </div>
   );
 };
